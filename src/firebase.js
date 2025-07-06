1b52c7daa140ded8f316dcae9e5547259704bc6d
mycitiverse <mycitiverse@gmail.com>
2025-07-06 22:20:05 +0530

    firebase new method

--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,15 @@
-// src/firebase.js
 const admin = require("firebase-admin");
 
-const serviceAccount = {
-  type: process.env.FB_TYPE,
-  project_id: process.env.FB_PROJECT_ID,
-  private_key_id: process.env.FB_PRIVATE_KEY_ID,
-  private_key: process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: process.env.FB_CLIENT_EMAIL,
-  client_id: process.env.FB_CLIENT_ID,
-  auth_uri: process.env.FB_AUTH_URI,
-  token_uri: process.env.FB_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER_CERT,
-  client_x509_cert_url: process.env.FB_CLIENT_CERT,
-};
+const serviceAccountJSON = Buffer.from(
+  process.env.FIREBASE_SERVICE_ACCOUNT,
+  "base64"
+).toString();
 
-console.log("PRIVATE KEY (parsed):\n", process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n"));
-
+const serviceAccount = JSON.parse(serviceAccountJSON);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
-
 module.exports = db;
