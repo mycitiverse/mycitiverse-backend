8629c46258f150fb3866b678161cba9848262965
mycitiverse <mycitiverse@gmail.com>
2025-07-06 22:00:38 +0530

    firebase error solving

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,7 @@ const serviceAccount = {
   type: process.env.FB_TYPE,
   project_id: process.env.FB_PROJECT_ID,
   private_key_id: process.env.FB_PRIVATE_KEY_ID,
-  private_key: process.env.FB_PRIVATE_KEY.split("\\n").join("\n"),
+  private_key: process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n"),
   client_email: process.env.FB_CLIENT_EMAIL,
   client_id: process.env.FB_CLIENT_ID,
   auth_uri: process.env.FB_AUTH_URI,
@@ -13,6 +13,9 @@ const serviceAccount = {
   auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER_CERT,
   client_x509_cert_url: process.env.FB_CLIENT_CERT,
 };
+
+console.log("PRIVATE KEY (parsed):\n", process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n"));
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
